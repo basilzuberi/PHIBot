1e0e3bc5806ae437cdf90fabbc0f4e283d95ee59
basilzuberi <basilzuberi@hotmail.com>
2019-06-22 19:30:33 -0400

    bot sends message

--- roleReact.js
+++ roleReact.js
@@ -1,11 +1,24 @@
-const Discord = require("discord.js");
-const { secret_token } = require("./config.json");
-const client = new Discord.Client();
+const { Client, Emoji, MessageReaction } = require("discord.js"); //imports for Client, emoji handling and reactions for discord.js
+const { secretToken, adminID } = require("./config.json"); //shh secret token is secret.
 const command = "!createphirole"; //initial command for setting up
-const adminID = "472830919242285066"; //only Moderator role can use command
+const client = new Client();
+
+
+
+
+client.login(secretToken);
 
 client.on("ready", () => {
+  //to check if bot is awake
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login(secret_token);
+//only send initial messages when admin says command
+client.on("message", msg => {
+  if (msg.member.roles.has(adminID) && msg.content.toLowerCase() == command) {
+    msg.channel.send("Hi, Please react with the :ok_hand: emoji to gain the PHI Role! You will be notified of any PHISOC Related activities going on. :)")
+
+    //after sending message, add reaction
+
+  }
+});
