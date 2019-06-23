1c2eb86ef202f420a45160410475bba1a720f2e1
basilzuberi <basilzuberi@hotmail.com>
2019-06-22 23:51:37 -0400

    REVAMP CODE [read description]
    Code now makes a static message entity be the centeral location for getting reaction. This message is Cached and will update roles as users click on the right emoji

--- roleReact.js
+++ roleReact.js
@@ -1,10 +1,6 @@
 const { Client, Emoji, MessageReaction } = require("discord.js"); //imports for Client, emoji handling and reactions for discord.js
-const { secretToken, adminID } = require("./config.json"); //shh secret token is secret.
-const command = "!createphirole"; //initial command for setting up
+const { secretToken, messageID } = require("./config.json"); //shh secret token is secret.
 const client = new Client();
-
-
-
 
 client.login(secretToken);
 
@@ -13,12 +9,49 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-//only send initial messages when admin says command
-client.on("message", msg => {
-  if (msg.member.roles.has(adminID) && msg.content.toLowerCase() == command) {
-    msg.channel.send("Hi, Please react with the :ok_hand: emoji to gain the PHI Role! You will be notified of any PHISOC Related activities going on. :)")
+//create a raw event handler for grabbing the user who reacted
 
-    //after sending message, add reaction
-
+client.on('raw', event => {
+  const eventName = event.t;
+  //check event handler for reaction add
+  if (eventName === 'MESSAGE_REACTION_ADD') {
+    if (event.d.message_id === messageID) {
+      var reactChannel = client.channels.get(event.d.channel_id);
+      //check if we ALREADY have cached the message we shall react to.
+      if (reactChannel.messages.has(event.d.message_id)) {
+        return;
+      } else {
+        //this will get the message if we haven't cached it. THEN it will grab the emoji
+        reactChannel.fetchMessage(event.d.message_id)
+          .then(msg => {
+            //get ok emoji reaction
+            var msgReaction = msg.reactions.get("👌");
+            //grab id of users who have reacted to the message.
+            var user = client.users.get(event.d.user_id);
+            //GOTO: messageReactionAdd event
+            client.emit('messageReactionAdd', msgReaction, user);
+          })
+          .catch(error => console.log(error));//log any errors with message handling (expected when no msg available).
+      }
+    }
   }
+});
+
+//handler for giving roles on reaction add
+
+client.on('messageReactionAdd', (messageReaction, user) => {
+
+  //find role with name of phiRole
+  var phiRole = "PHI";
+  var role = messageReaction.message.guild.roles.find(role => role.name.toLowerCase() === phiRole.toLowerCase());
+
+
+  if (role) {
+    //when we found the role, we need to get the user's id to add the role.
+    var member = messageReaction.message.guild.members.find(member => member.id === user.id);
+    if (member) {
+      member.addRole(role.id);
+    }
+  }
+
 });
