986caffad44d2ba39683892efd4fff88f964d441
Vincent Nguyen <v.ngu@outlook.com>
2020-02-08 19:18:23 -0500

    i forgot to add node modules

--- courseScraper.js
+++ courseScraper.js
@@ -14,28 +14,32 @@ function scrapeCourse(courseID) {
 }
 
 // Queries the Laurier academic calendar for the URL for the specified course.
-var _getCourseURL = (courseID) => {
-    url = "https://academic-calendar.wlu.ca/search.php?s_text={$courseID}&cal=1&y=79&s_levels%5B%5D=1&s_levels%5B%5D=2&s_levels%5B%5D=3&s_levels%5B%5D=4&submit.x=8&submit.y=10";
+function _getCourseURL (courseID) {
+    url = `https://academic-calendar.wlu.ca/search.php?s_text=${courseID}&cal=1&y=79&s_levels%5B%5D=1&s_levels%5B%5D=2&s_levels%5B%5D=3&s_levels%5B%5D=4&submit.x=8&submit.y=10`;
     
     axios(url)
-    .then(function(response){
+    .then((response) => {
             const courseHTML = response.data;
             const $ = cheerio.load(courseHTML);
 
-            // Returns first link in the list of courses if the course showed up at the top of the searchr esults.
-            if ($('.res > dl > dt > a > b').text().contains(courseID)) {
+            // Returns first link in the list of courses if the course showed up at the top of the search results.
+            if ($('.res > dl > dt > a > b').text().includes(courseID)) {
                 return $('.res > dl > dt > a').attr('href');
-            }   
+            }
+    })
+    .catch(() => {
+        console.log("couldn't get a response")
+        return null;
     });
 
-    return null;
+    
 }
 
 // Grabs the course info and returns a JSON with its info.
-var  _getCourseInfo = (courseID) => {    
+function  _getCourseInfo (courseID) {    
     courseURL = _getCourseURL(courseID);
-
-    if (course) {
+    console.log(courseURL);
+    if (courseURL) {
         axios(courseURL)
         .then((response) => {
             const $ = cheerio.load(cheerio.load(response.data));
@@ -49,6 +53,8 @@ var  _getCourseInfo = (courseID) => {
         });
     }
 
+    console.log("Could not get the course URL for " + courseID);
     return null;
 }
 
+console.log(_getCourseInfo('CP104'));
