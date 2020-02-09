2020d86aa45f300140b558f2d7496b879f7caac4
Vincent Nguyen <v.ngu@outlook.com>
2020-02-08 20:16:56 -0500

    ok it works now

--- courseScraper.js
+++ courseScraper.js
@@ -1,60 +1,60 @@
+/* Laurier Course Scraper
+ * Author: nvplus
+ * Description: Queries the Laurier academic calendar for undergraduate courses.
+ */
+
 const cheerio = require('cheerio'); 
 const axios = require('axios');
+const year = 79; //This is the year code for 2020. TODO: Let you search different years
 
-function scrapeCourse(courseID) {
-    return new Promise((resolve, reject) => {
-        const courseInfo = _getCourseInfo(courseID);
+/* Scrapes a Laurier course
+ * @param {String} courseID (e.g. CP312, GG231, ST259)
+ * @return {Promise} A promise that resolves to an object {id, title, description, requirements, exclusions}
+ */
+async function scrapeCourse(courseID) {
+    courseURL = await _getCourseURL(courseID);
+    courseInfo = await _getCourseInfo(courseURL);
 
-        if (courseInfo) {
-            resolve(courseInfo);
-        }
-
-        reject("Unable to query the course.");
-    });
+    return courseInfo;
 }
 
 // Queries the Laurier academic calendar for the URL for the specified course.
 function _getCourseURL (courseID) {
-    url = `https://academic-calendar.wlu.ca/search.php?s_text=${courseID}&cal=1&y=79&s_levels%5B%5D=1&s_levels%5B%5D=2&s_levels%5B%5D=3&s_levels%5B%5D=4&submit.x=8&submit.y=10`;
-    
-    axios(url)
-    .then((response) => {
+    url = `https://academic-calendar.wlu.ca/search.php?s_text=${courseID}&cal=1&y=${year}&s_levels%5B%5D=1&s_levels%5B%5D=2&s_levels%5B%5D=3&s_levels%5B%5D=4&submit.x=8&submit.y=10`;
+
+    return new Promise((resolve, reject) => {
+        axios.get(url).then((response) => {
             const courseHTML = response.data;
             const $ = cheerio.load(courseHTML);
-
-            // Returns first link in the list of courses if the course showed up at the top of the search results.
-            if ($('.res > dl > dt > a > b').text().includes(courseID)) {
-                return $('.res > dl > dt > a').attr('href');
-            }
-    })
-    .catch(() => {
-        console.log("couldn't get a response")
-        return null;
-    });
-
     
+            // If the course showed up at the top of the search, return its URL.
+            if ($('.res > dl > dt > a > b').eq(0).text().toLowerCase().includes(courseID.toLowerCase())) {
+                resolve("https://academic-calendar.wlu.ca/" + $('.res > dl > dt > a').eq(0).attr('href'));
+            }
+
+            reject("Course was unable to be found.");
+        });
+    });
 }
 
 // Grabs the course info and returns a JSON with its info.
-function  _getCourseInfo (courseID) {    
-    courseURL = _getCourseURL(courseID);
-    console.log(courseURL);
-    if (courseURL) {
-        axios(courseURL)
-        .then((response) => {
-            const $ = cheerio.load(cheerio.load(response.data));
+function _getCourseInfo (courseURL) { 
+    return new Promise((resolve, reject) => {
 
-            return {
-                title: $('h1 > span').text(),
-                description: $('p'),
+        axios.get(courseURL)
+        .then((response) => {
+            const $ = cheerio.load(response.data);
+            
+            resolve ({
+                title: $('h1 > span').eq(0).text(),
+                description: $('p').text(),
                 required: $('.required').text(),
                 exclusions: $('.exclusions').text()
-            }
-        });
-    }
+            });
 
-    console.log("Could not get the course URL for " + courseID);
-    return null;
+            reject("error");
+        });
+    });
 }
 
-console.log(_getCourseInfo('CP104'));
+scrapeCourse('cp104').then(result => console.log(result));
